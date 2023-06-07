571b91be0d7a648c5fa89738617789975091a998
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-06-07 19:00:39 +0530

    project entity

---
+++ spring_crud_project_simple/src/main/java/com/mp/project/spring_crud_project_simple/Entity/Project.java
@@ -0,0 +1,49 @@
+package com.mp.project.spring_crud_project_simple.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String title;
+    private String Coordinator;
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getTitle () {
+        return title;
+    }
+
+    public void setTitle (String title) {
+        this.title = title;
+    }
+
+    public String getCoordinator () {
+        return Coordinator;
+    }
+
+    public void setCoordinator (String coordinator) {
+        Coordinator = coordinator;
+    }
+
+    @Override
+    public String toString () {
+        return "project{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", Coordinator='" + Coordinator + '\'' +
+                '}';
+    }
+}
