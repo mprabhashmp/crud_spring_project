6d0cbf8301b9ca39a52ac3dae74146bf3162504a
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-06-07 18:59:44 +0530

    create spring

---
+++ spring_crud_project_simple/src/test/java/com/mp/project/spring_crud_project_simple/SpringCrudProjectSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.project.spring_crud_project_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudProjectSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
