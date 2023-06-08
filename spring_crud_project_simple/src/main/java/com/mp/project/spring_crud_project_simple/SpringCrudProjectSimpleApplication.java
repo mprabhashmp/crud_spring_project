35455ea5f75639a8e67c344c63fae6a98c07d630
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-06-08 17:42:25 +0530

    crud

---
+++ spring_crud_project_simple/src/main/java/com/mp/project/spring_crud_project_simple/SpringCrudProjectSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.mp.project.spring_crud_project_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudProjectSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudProjectSimpleApplication.class, args);
+	}
+
+}
